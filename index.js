151efe2a807e826f9b4e99708187cbd3f895be29
ruhulamin111 <ruhuljuwel111@gmail.com>
2022-07-26 08:19:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// variable declare with 5 steps: 
+// 1. variable declare with var  
+// 2. variable meaningful name 
+// 3. set the = sign 
+// 4. variable value numeric, string or boolean 
+// 5. use semicolon
+
+// variable declare string type 
+var name = 'Abdur Rahim Khandokar';
+
+// numeric type variable 
+var age = 25;
+
+// boolean type variable 
+var married = true;
+console.log(married);
+
+// variable with camelCase 
+var permanentAddress = 'Mirpur, Dhaka';
+var office90 = 'street number';
+
+// mathmatical opearation 
+var mobilePrice = 5000;
+var laptopPrice = 70000;
+var watchPrice = 3000;
+var availableMoney = 100000;
+var mobile = 10;
+
+var totalPrice = mobilePrice + laptopPrice + watchPrice;
+console.log(totalPrice);
+
+var restMoney = availableMoney - totalPrice;
+console.log(restMoney);
+
+var totalMobilePrice = mobile * mobilePrice;
+console.log(totalMobilePrice);
+
+var totalMobile = availableMoney / mobilePrice;
+console.log(totalMobile);
